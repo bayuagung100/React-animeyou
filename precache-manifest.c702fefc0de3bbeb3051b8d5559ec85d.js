1cbd10cf16114b8bae9fadc6e0ecaf2ec99eed65
bayuagung100 <bayuagung100@gmail.com>
2020-01-22 15:52:47 +0700

    Updates

---
+++ precache-manifest.c702fefc0de3bbeb3051b8d5559ec85d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "be6e0f45302e293d29d6711df3479696",
+    "url": "/React-animeyou/index.html"
+  },
+  {
+    "revision": "5b2143e06b474ec3dcd3",
+    "url": "/React-animeyou/static/css/main.07bc9191.chunk.css"
+  },
+  {
+    "revision": "9098501c3c86d47477ee",
+    "url": "/React-animeyou/static/js/2.9e2f73ab.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/React-animeyou/static/js/2.9e2f73ab.chunk.js.LICENSE"
+  },
+  {
+    "revision": "5b2143e06b474ec3dcd3",
+    "url": "/React-animeyou/static/js/main.6469f03a.chunk.js"
+  },
+  {
+    "revision": "a2cde632c0785ab35137",
+    "url": "/React-animeyou/static/js/runtime-main.a1d58860.js"
+  }
+]);
